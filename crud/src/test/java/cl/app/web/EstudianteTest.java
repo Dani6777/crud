8977a31a68c73b12a9be772c42c6146ae8880e39
Daniel Morales <daniel.morales23@outlool.cl>
2023-12-13 23:15:39 -0300

    Estudiantes test

--- crud/src/test/java/cl/app/web/EstudianteTest.java
+++ crud/src/test/java/cl/app/web/EstudianteTest.java
@@ -1,4 +1,24 @@
 package cl.app.web;
 
-public class EstudiantesTest {
+
+import cl.app.web.entity.Estudiante;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EstudianteTest {
+
+    @Test
+    public void testEstudinte(){
+        Estudiante estudiante = new Estudiante();
+        estudiante.setId(1L);
+        estudiante.setNombre("John");
+        estudiante.setApellido("Doe");
+        estudiante.setCorreo("john.doe@example.com");
+
+        assertEquals(Long.valueOf(1L), estudiante.getId());
+        assertEquals("John", estudiante.getNombre());
+        assertEquals("Doe", estudiante.getApellido());
+        assertEquals("john.doe@example.com", estudiante.getCorreo());
+    }
 }
